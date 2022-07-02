7107d608ccabaab5af225920c8e2289d486eaaa5
FahadulShadhin <fahadulshadhin17@gmail.com>
2022-07-02 09:28:42 +0600

    initial commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const solveButton = document.querySelector("#solve-button")
 const squares = 81
 let board = []
 
+
 for (let i=0; i<squares; i++) {
     const inputElement = document.createElement("input")
     inputElement.setAttribute('type', 'number')
